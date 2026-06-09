decf5a0775ecc3a7f9f71325919e20af7eb0bdc7
BryanJericho <bryanpanggalo@gmail.com>
2026-06-09 19:05:39 +0800

    fix: layouting and google's auth

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9domaspmafg"
+    "revision": "0.ds1co3ht90g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
